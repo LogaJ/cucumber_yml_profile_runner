3d62667d37fe28a04c91a5256d7113edc33adb47
Loga Jegede <loga.jegede@net-a-porter.com>
2012-03-04 10:41:50 +0000

    now able to handle localhost and remote hosts.

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,6 +3,11 @@ require 'capybara/cucumber'
 web_host = YAML.load_file "./config/#{ENV['APP']}.settings.yml"
 
 Capybara.run_server = false
-Capybara.app_host = "http://www.#{web_host[:host]}.com"
 Capybara.default_selector = :css
 Capybara.default_driver = :selenium
+
+if web_host.has_key?(:port)
+  Capybara.app_host = "http://#{web_host[:host]}:#{web_host[:port]}"
+else
+  Capybara.app_host = "http://www.#{web_host[:host]}.com"
+end
